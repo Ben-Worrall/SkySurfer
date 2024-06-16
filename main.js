4b376b3302fa994bf96d8237e96160971e9b226b
Ben <benworrallpc@gmail.com>
2024-06-16 18:40:14 +0200

    gravity, keybinds

---
+++ main.js
@@ -0,0 +1,84 @@
+class Game {
+    constructor(canvas, context){
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.baseHeight = 2000
+        this.ratio = this.height / this.baseHeight
+        this.player = new Player(this)
+
+        //gravity pulls player down 1 pixel per frame
+        this.gravity = 1
+        
+
+        this.resize(window.innerWidth, window.innerHeight)
+
+        //for any viewport on any device
+        window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight)
+        })
+
+        //mouse controls
+        this.canvas.addEventListener('mousedown', (e) => {
+              this.player.fly()
+        })
+        //keyboard controls
+        window.addEventListener('keydown', e => {
+            if(e.key == ' ' || e.key === 'Enter' || e.key === "w")[
+                this.player.fly()
+            ]
+        })
+        //touch controls
+        this.canvas.addEventListener('touchstart', e => {
+            this.player.fly()
+        })
+
+
+
+    }
+
+    resize(width, height){
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.ctx.fillStyle = 'red'
+        this.ratio = this.height / this.baseHeight
+
+        
+        //for scaling depnding on height
+        this.gravity = 0.5 * this.ratio
+        this.player.resize()
+    }
+
+    render(){
+        //coords and size for player
+        
+        this.player.update()
+        this.player.draw()
+        
+    }
+
+
+}
+
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('GameCanvas')
+    const ctx = canvas.getContext('2d');
+    canvas.width = 600
+    canvas.height = 600
+    ctx.fillStyle = 'red'
+
+    const game = new Game(canvas, ctx)
+    
+
+
+    //run function to update all draw objects then delete then run agina
+    function animate(){
+        ctx.clearRect(0,0, canvas.height, canvas.width)
+        game.render()
+        requestAnimationFrame(animate)
+    }
+    requestAnimationFrame(animate)
+})
---
+++ scripts/player.js
@@ -0,0 +1,57 @@
+class Player{
+    constructor(game){
+        this.game = game;
+        this.x = 50;
+        this.y
+        this.width
+        this.height
+        //increase speed of gravity
+        this.speedY 
+
+        //sprite picture size
+        this.spriteWidth = 200
+        this.spriteHeight = 200
+
+        //flying speed
+        this.flySpeed
+        
+    }
+
+    draw(){
+        this.game.ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+    update(){
+        this.y += this.speedY
+        //gravity
+        if(!this.isTouchingBottom()){
+            this.speedY += this.game.gravity
+        }
+
+        //bottom boundary
+        if(this.isTouchingBottom()){
+            this.y = this.game.height - this.height
+        }
+    }
+
+    //for scaling depnding on height
+    resize(){
+        this.width = this.spriteWidth * this.game.ratio
+        this.height = this.spriteHeight * this.game.ratio
+        this.y = this.game.height * 0.5 - this.height * 0.5
+        this.speedY = -8 * this.game.ratio
+        this.flySpeed = 20 * this.game.ratio
+    }
+    isTouchingTop(){
+        return this.y <= 0
+    }
+    isTouchingBottom(){
+        return  this.y >= this.game.height - this.height
+    }
+    fly(){
+        if(!this.isTouchingTop())[
+            this.speedY = -this.flySpeed
+        ]
+        
+    }
+
+}
