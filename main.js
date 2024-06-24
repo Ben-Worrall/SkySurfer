49f73374b423be6bb7647eb5bb91ca0413b5db5a
Ben <benworrallpc@gmail.com>
2024-06-24 12:48:10 +0200

    getting score up

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ class Game {
         this.player = new Player(this)
         //for sea obstacles
         this.SeaObstacles = []
-        this.numberOfObstacles = 10
+        this.numberOfObstacles = 2000
+        this.score
+        this.gameOver
 
         //gravity pulls player down 1 pixel per frame
         this.gravity = 0.5
@@ -56,9 +58,11 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.ctx.fillStyle = 'red'
+        this.ctx.font = '10px Bungee'
+        
         this.ratio = this.height / this.baseHeight
 
-        this.speed = 6 * this.ratio
+        this.speed = 5 * this.ratio
         //for scaling depnding on height
         this.gravity = 0.5 * this.ratio
         this.background.resize()
@@ -68,14 +72,18 @@ class Game {
         this.SeaObstacles.forEach(obstacle => {
             obstacle.resize()
         })
+        this.score = 0;
+        this.gameOver = false
     }
 
     render(){
+        
         //coords and size for player
         this.background.update()
         this.BackgroundWater.update()
         this.background.draw()
         this.BackgroundWater.draw()
+        this.drawStatusText()
         this.player.update()
         this.player.draw()
         //
@@ -91,16 +99,26 @@ class Game {
         this.SeaObstacles = []
         //first position for obstacle
         const firstX = 100
-        const obstacleSpacing = 2000 * this.ratio;
+        const obstacleSpacing = 3500 * this.ratio;
         for(let i =0; i < this.numberOfObstacles; i++){
             this.SeaObstacles.push(new SeaObstacle(this, firstX + i * obstacleSpacing))
         }
     }
 
 
+    drawStatusText(){
+        this.ctx.save()
+        this.ctx.fillText('score: ' + this.score, ((this.width  * 0.5)), 30)
+        this.ctx.restore()
+    }
+
 
 
 }
+
+
+
+
 
 window.addEventListener('load', function(){
     const canvas = this.document.getElementById('GameCanvas')
@@ -117,7 +135,7 @@ window.addEventListener('load', function(){
     function animate(){
         ctx.clearRect(0,0, canvas.height, canvas.width)
         game.render()
-        requestAnimationFrame(animate)
+        if(!game.gameOver) requestAnimationFrame(animate)
     }
     requestAnimationFrame(animate)
 
@@ -130,7 +148,7 @@ window.addEventListener('load', function(){
         "./assets/background/water/BACKGROUND WATER 3.png",
         "./assets/background/water/BACKGROUND WATER 4.png"
     ]
-    var LoadedBGwaterAR = []
+    
     for(let i =0; i< BGwaterAR.length; i++){
         var img=new Image();
         img.src=BGwaterAR[i];
@@ -143,7 +161,7 @@ window.addEventListener('load', function(){
  
 function draw() {
     setTimeout(function() {
-        requestAnimationFrame(draw);
+        if(!game.gameOver) requestAnimationFrame(draw);
         document.getElementById('BackgroundWaterDiv').src =  BGwaterAR[count]
         
         count++;
--- scripts/SeaObstacles.js
+++ scripts/SeaObstacles.js
@@ -1,17 +1,25 @@
 class SeaObstacle {
     constructor(game, x){
         this.game = game
-        this.spriteWidth = 150
-        this.spriteHeight = 150
+        this.spriteWidth = 350
+        this.spriteHeight = 250
         this.scaledWidth = this.spriteWidth * this.game.ratio *0.01
         this.scaledHeight = this.spriteHeight * this.game.ratio *0.01
         this.x = x;
-        this.y = this.game.height * 0.8;
+        this.y = this.game.height * 0.76;
 
     }
     update(){
-        this.x -= this.game.speed + 0.5
+        //randomise how fast they move
+        this.x -= this.game.speed +( (Math.random() * 0.1) + 1)
+        //check if its off the screen
+        if (this.isOffScreen()){
+            this.markedForDeletion = true
+            this.game.SeaObstacles = this.game.SeaObstacles.filter(SeaOb => !SeaOb.markedForDeletion);
+            console.log(this.game.SeaObstacles.length)
 
+            this.game.score++
+        }
     }
     draw(){
         this.game.ctx.fillRect( this.x, this.y, this.scaledWidth, this.scaledHeight)
@@ -20,6 +28,9 @@ class SeaObstacle {
     resize(){
         this.scaledWidth = this.spriteWidth * this.game.ratio
         this.scaledHeight = this.spriteHeight * this.game.ratio
+    }
+    isOffScreen(){
+         return this.x < -this.scaledWidth
     }
 
     
